01e1b0668c0d7d5d7cd1e65c67384d4b002cdca0
Kyrie7 <missanswer@163.com>
2020-10-27 17:58:20 +0800

    损失函数增加Relativistic Standard GAN，Relativistic average Standard GAN

--- losses.py
+++ losses.py
@@ -7,6 +7,8 @@ def l1_loss(real_image, gen_image, l1_lambda):
     return l1_lambda * loss
 
 
+
+# Standard GAN
 def standard_generator_loss(disc_gen):
     """standard gan generator loss
 
@@ -19,9 +21,76 @@ def standard_generator_loss(disc_gen):
 
 
 def standard_discriminator_loss(disc_real, disc_gen):
+    """standard gan discriminator loss
+
+    Args:
+        disc_real: Discriminator judge images that is real
+        disc_gen: Discriminator judge images that generated from Generator
+
+    """
+
     loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
     real_loss = loss_obj(tf.ones_like(disc_real), disc_real)
     gen_loss = loss_obj(tf.zeros_like(disc_gen), disc_gen)
     return (real_loss + gen_loss) * 0.5
+
+
+# Relativistic Standard GAN
+def relativistic_standard_generator_loss(disc_real, disc_gen):
+    """relativistic standard generator loss
+
+    Args:
+        disc_real: Discriminator judge images that is real
+        disc_gen: Discriminator judge images that generated from Generator
+
+    """
+    loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    return loss_obj(tf.ones_like(disc_real), (disc_gen - disc_real))
+
+
+def relativistic_standard_discriminator_loss(disc_real, disc_gen):
+    """relativistic standard discriminator loss
+
+    Args:
+        disc_real: Discriminator judge images that is real
+        disc_gen: Discriminator judge images that generated from Generator
+
+    """
+    loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    return loss_obj(tf.ones_like(disc_real), (disc_real - disc_gen))
+
+
+
+# Relativistic Average Standard GAN - RaSGAN
+def relativistic_average_standard_generator_loss(disc_real, disc_gen):
+    """relativistic average standard generator loss
+
+    Args:
+        disc_real: Discriminator judge images that is real
+        disc_gen: Discriminator judge images that generated from Generator
+
+    """
+
+    loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    real_loss = loss_obj(tf.zeros_like(disc_real), disc_real - tf.reduce_mean(disc_gen))
+    gen_loss = loss_obj(tf.ones_like(disc_gen), disc_gen - tf.reduce_mean(disc_real))
+    return (real_loss + gen_loss) / 2
+
+
+def relativistic_average_standard_discriminator_loss(disc_real, disc_gen):
+    """relativistic average standard discriminator loss
+
+    Args:
+        disc_real: Discriminator judge images that is real
+        disc_gen: Discriminator judge images that generated from Generator
+
+    """
+
+    loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    real_loss = loss_obj(tf.ones_like(disc_real), disc_real - tf.reduce_mean(disc_gen))
+    gen_loss = loss_obj(tf.zeros_like(disc_gen), disc_gen - tf.reduce_mean(disc_real))
+    return (real_loss + gen_loss) / 2
+
+
 
 
